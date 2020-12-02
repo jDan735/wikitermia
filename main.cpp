64cdb16a99c64d81d41ff2996cd74e1f30d5802e
jDan735 <daniel734@bk.ru>
2020-12-02 22:24:21 +0300

    Minimal curl app

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <curl/curl.h>
+
+using namespace std;
+
+int main(void){
+
+    cout << "test" << endl;
+
+    CURL *curl;
+    CURLcode res;
+
+    curl = curl_easy_init();
+    if(curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "http://ru.wikipedia.org/w/api.php?action=query&prop=extracts&list=search&srsearch=%D0%BA%D0%B0%D1%86&srlimit=10&srprop=size&format=json");
+
+        /* example.com is redirected, so we tell libcurl to follow redirection */
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+        /* Perform the request, res will get the return code */
+        res = curl_easy_perform(curl);
+        /* Check for errors */
+        if(res != CURLE_OK)
+            cout << "curl_easy_perform() failed:" << curl_easy_strerror(res) << endl;
+
+        /* always cleanup */
+        curl_easy_cleanup(curl);
+    }
+
+    return 0;
+}
