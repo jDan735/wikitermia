bad96d0aa48f66a2211f91a84f394238e392673d
jDan735 <daniel734@bk.ru>
2020-12-04 01:52:59 +0300

    Write search and getPage

--- main.cpp
+++ main.cpp
@@ -1,29 +1,22 @@
+#include <nlohmann/json.hpp>
 #include <iostream>
-#include <curl/curl.h>
+#include "main.h"
 
 using namespace std;
+using namespace nlohmann;
 
-int main(void){
-    CURL *curl;
-    CURLcode res;
+int main(int argc, char *argv[]){
+    Wikipedia wiki;
 
-    curl = curl_easy_init();
-    if(curl) {
-        curl_easy_setopt(curl, CURLOPT_CAINFO, "cacert.pem");
-        curl_easy_setopt(curl, CURLOPT_URL, "http://ru.wikipedia.org/w/api.php?action=query&prop=extracts&list=search&srsearch=%D0%BA%D0%B0%D1%86&srlimit=10&srprop=size&format=json");
-
-        /* example.com is redirected, so we tell libcurl to follow redirection */
-        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-
-        /* Perform the request, res will get the return code */
-        res = curl_easy_perform(curl);
-        /* Check for errors */
-        if(res != CURLE_OK)
-            cout << "curl_easy_perform() failed:" << curl_easy_strerror(res) << endl;
-
-        /* always cleanup */
-        curl_easy_cleanup(curl);
+    if (argc < 2) {
+        cout << "Enter your wikipedia page request!" << endl;
+        return 1;
     }
+
+    wiki.setLang("en");
+    //json res = wiki.search("Бан", "10");
+
+    cout << wiki.getPage(argv[1]) << endl;
 
     return 0;
 }
